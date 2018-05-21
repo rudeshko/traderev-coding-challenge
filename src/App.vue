b69b6a4f78cb5a14c22843d539739aae674b178b
Dima Rudeshko <dima@rudeshko.net>
2018-05-21 09:48:52 -0400

    Added the main file structure

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        Testing
+    </div>
+</template>
+
+<script>
+import unsplash from './unsplash.js';
+
+export default {
+    name: 'app',
+    data: () => ({
+        photos: [],
+        currentPage: 1
+    }),
+    created() {
+        unsplash.getPhotos();
+    }
+}
+</script>
+
+<style lang="scss">
+</style>
